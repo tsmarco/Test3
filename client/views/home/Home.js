0d564b3168e01eff0e0cf8f64062b08a4c650d71
mt <mt@mt.com>
2018-03-14 00:09:21 +1000

    Implemented basic entities frontend.

--- client/views/home/Home.js
+++ client/views/home/Home.js
@@ -1,44 +1,96 @@
-/**
- * ASP.NET Core Starter Kit (https://dotnetreact.com)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React, { PropTypes } from 'react';
 import Layout from '../../components/Layout';
 
-const title = 'ASP.NET Core Starter Kit';
-const link = 'https://github.com/kriasoft/aspnet-starter-kit';
+const title = 'ASP.NET Core API';
+const link = '';
 
 class Home extends React.Component {
 
   static propTypes = {
-    articles: PropTypes.array.isRequired,
+    articles: PropTypes.array,
   };
+  constructor(props){
+    super(props);
 
-  componentDidMount() {
-    document.title = title;
+    this.state = {
+      id:'',
+      value:'',
+      entities:[
+            ]
+          };
+
+    this.idChanged = this.idChanged.bind(this);
+    this.valueChanged = this.valueChanged.bind(this);
+    this.getEntitiesList = this.getEntitiesList.bind(this);
   }
 
-  render() {
-    return (
-      <Layout>
-        <h1 className="mdl-typography--title">Welcome to {title}!</h1>
+    componentDidMount() {
+      document.title = title;
+      this.getEntitiesList();
+    }
+
+    getEntitiesList(){
+      //Fetch entities list
+      fetch('http://localhost:3000/api/entities/')
+      .then((response) => response.json())
+      .then((responseJson) => {
+          this.setState({ entities: responseJson });
+        });
+    }
+
+    idChanged(event){
+      var newId = event.target.value;
+      //fetch single entity.
+      fetch('http://localhost:3000/api/entities/'+newId)
+      .then((response) => response.json())
+      .then((responseJson) => {
+          this.setState({value: responseJson.value,id: newId});
+        });
+    }
+
+    valueChanged(event){
+      //Post single entity.
+      var newValue = event.target.value;
+      fetch('http://localhost:3000/api/entities/'+this.state.id, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          value: newValue
+        })
+      }).then((response) => response.json())
+      .then((responseJson) => {
+          this.getEntitiesList();
+        });
+    }
+
+    render() {
+      return (<Layout>
+        <h1 className="mdl-typography--title">Welcome to API!</h1>
         <p className="mdl-typography--body-1">
-          For more information visit <a href={link}>{link}</a>
+          <label>
+            Query:
+            <input type="text" name="id" value={this.state.id} onChange={this.idChanged} />
+          </label>
         </p>
-        <h4 className="mdl-typography--title">Articles</h4>
+        <p className="mdl-typography--body-1">
+          <label>
+            Value:
+            <input type="text" name="id" value={this.state.value} onChange={this.valueChanged} disabled = {(this.state.id == '')? "disabled" : ""}/>
+          </label>
+        </p>
+
         <ul>
-          {this.props.articles.map((article, i) =>
-            <li key={i}><a href={article.url}>{article.title}</a> by {article.author}</li>
+          {this.state.entities.map((entity, i) =>
+          <li key={i}><a >{entity.key}</a> - {entity.value}</li>
           )}
         </ul>
       </Layout>
-    );
-  }
+      );
+    }
+
 }
 
 export default Home;
